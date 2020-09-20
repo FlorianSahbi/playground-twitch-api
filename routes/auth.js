c697b8f525638b2399c42d03fa90232a79e69308
FlorianSahbi <florian.sahbi@gmail.com>
2020-09-20 18:59:26 +0200

    Add heroku callback

--- routes/auth.js
+++ routes/auth.js
@@ -42,7 +42,7 @@ router.get('/sub', async (req, res) => {
 });
 
 router.get('/confirm', (req, res) => {
-  console.log("confirm -->", req.query);
+  console.log("confirm -->", req.query[0]);
   res.status(200).send("ok")
 })
 
