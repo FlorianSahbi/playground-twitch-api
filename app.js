c594b159cf0f7c56591e4a829d282fe82e434b73
FlorianSahbi <florian.sahbi@gmail.com>
2020-09-22 03:15:42 +0200

    clean code

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/twitch');
-var autheRouter = require('./routes/auth');
 
 var app = express();
 
@@ -21,7 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/twitch', authRouter);
-app.use('/auth', autheRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
